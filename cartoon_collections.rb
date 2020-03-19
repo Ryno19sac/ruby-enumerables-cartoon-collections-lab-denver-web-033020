659c5f6a492bd412c3de6ec7b54a3f8a6de55d9c
Ryan Anderson <ryan.t.anderson19@gmail.com>
2020-03-19 17:37:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,7 +22,8 @@ roll_call_dwarves(dwarves)
 planeteer_calls = %w[earth wind fire water heart]
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.map {|calls| calls.capitalize
+  planeteer_calls.map {|calls| calls.capitalize + '!'
+  puts planeteer_calls
   
   # Your code here
 end
