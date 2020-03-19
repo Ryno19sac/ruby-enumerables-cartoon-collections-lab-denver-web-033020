e481c06d9700d948dfd2a7d0a4c84a3d0216f6e9
Ryan Anderson <ryan.t.anderson19@gmail.com>
2020-03-19 17:45:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -29,8 +29,10 @@ end
   # Your code here
 
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls_long)
+  
+  
+  
 end
 
 def find_the_cheese# code an argument here
